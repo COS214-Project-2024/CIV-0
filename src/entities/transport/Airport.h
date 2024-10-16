707252ae5121b7eb98292f68c888b4c043a2b7c9
RKruse42 <1richardkruse@gmail.com>
2024-10-16 14:57:53 +0200

    Finished Road and Transport without observer updates

--- src/entities/transport/Airport.h
+++ src/entities/transport/Airport.h
@@ -7,7 +7,10 @@ class Airport : public Transport
 {
 public:
     Airport();
+    Airport(int electricity, int water, std::string symbol, int radius, int localEffect, int globalEffect, int width, int height, int revenue, Size size, int xPos, int yPos, int buildTime);
     ~Airport();
+    void update(); //TODO
+    Entity* clone();
 };
 
 #endif // AIRPORT_H
