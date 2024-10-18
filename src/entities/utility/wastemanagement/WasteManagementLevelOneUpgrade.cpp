643ffedb5dded6df5232aa54c4b1b4997c279ec1
Reece Jordaan <treesnek@gmail.com>
2024-10-18 15:01:55 +0200

    Implemented WasteManagement decorator

--- src/entities/utility/wastemanagement/WasteManagementLevelOneUpgrade.cpp
+++ src/entities/utility/wastemanagement/WasteManagementLevelOneUpgrade.cpp
@@ -1,4 +1,25 @@
 #include "WasteManagementLevelOneUpgrade.h"
 
-WasteManagementLevelOneUpgrade::WasteManagementLevelOneUpgrade() {}
-WasteManagementLevelOneUpgrade::~WasteManagementLevelOneUpgrade() {}
+WasteManagementLevelOneUpgrade::WasteManagementLevelOneUpgrade(WasteManagement* wasteManagement) : WasteManagementUpgrade(wasteManagement) {
+
+}
+
+WasteManagementLevelOneUpgrade::WasteManagementLevelOneUpgrade(WasteManagementLevelOneUpgrade* wasteManagementLevelOneUpgrade) : WasteManagementUpgrade(wasteManagementLevelOneUpgrade) {
+
+}
+
+WasteManagementLevelOneUpgrade::~WasteManagementLevelOneUpgrade() {
+
+}
+
+void WasteManagementLevelOneUpgrade::update() {
+    wasteManagement->update();
+}
+
+Entity* WasteManagementLevelOneUpgrade::clone() {
+    return new WasteManagementLevelOneUpgrade(this);
+}
+
+int WasteManagementLevelOneUpgrade::getOutput() {
+    return wasteManagement->getOutput() * UPGRADE;
+}
--- src/entities/utility/wastemanagement/WasteManagementLevelTwoUpgrade.h
+++ src/entities/utility/wastemanagement/WasteManagementLevelTwoUpgrade.h
@@ -3,11 +3,60 @@
 
 #include "WasteManagementUpgrade.h"
 
+/**
+ * @class WasteManagementLevelTwoUpgrade
+ * @brief Represents the second level upgrade to a WasteManagement entity.
+ *
+ * The WasteManagementLevelTwoUpgrade class enhances the base functionality of a WasteManagement system by
+ * increasing its output. This is the second upgrade level in the series.
+ */
 class WasteManagementLevelTwoUpgrade : public WasteManagementUpgrade
 {
 public:
-    WasteManagementLevelTwoUpgrade();
+    /**
+     * @brief Constructs a WasteManagementLevelTwoUpgrade object.
+     * 
+     * Enhances the specified WasteManagement system with a level two upgrade.
+     * 
+     * @param wasteManagement Pointer to the original WasteManagement to be upgraded.
+     */
+    WasteManagementLevelTwoUpgrade(WasteManagement* wasteManagement);
+
+    /**
+     * @brief Copy constructor for WasteManagementLevelTwoUpgrade.
+     * 
+     * Copies the attributes of an existing WasteManagementLevelTwoUpgrade object.
+     * 
+     * @param wasteManagementLevelTwoUpgrade Pointer to the existing object to be copied.
+     */
+    WasteManagementLevelTwoUpgrade(WasteManagementLevelTwoUpgrade* wasteManagementLevelTwoUpgrade);
+
+    /**
+     * @brief Destructor for WasteManagementLevelTwoUpgrade.
+     */
     ~WasteManagementLevelTwoUpgrade();
+
+    /**
+     * @brief Updates the state of the upgraded waste management system.
+     */
+    void update();
+
+    /**
+     * @brief Clones the current WasteManagementLevelTwoUpgrade object.
+     * 
+     * @return A pointer to the newly cloned object.
+     */
+    Entity* clone();
+
+    /**
+     * @brief Retrieves the upgraded waste management system's output.
+     * 
+     * @return The updated output with a 2.625x enhancement.
+     */
+    int getOutput();
+
+private:
+    const int UPGRADE = 2.625; ///< Output multiplier for the level two upgrade.
 };
 
 #endif // WASTEMANAGEMENTLEVELTWOUPGRADE_H
