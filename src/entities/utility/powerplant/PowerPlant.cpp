9cadd2ee86c7cfc7d7c520f450434b859a7c0bf5
Reece Jordaan <treesnek@gmail.com>
2024-10-18 09:55:06 +0200

    Made additions to PowerPlant

--- src/entities/utility/powerplant/PowerPlant.cpp
+++ src/entities/utility/powerplant/PowerPlant.cpp
@@ -1,20 +1,21 @@
 #include "PowerPlant.h"
 
-PowerPlant::PowerPlant() {}
+PowerPlant::PowerPlant(int electricity, int water, std::string symbol, int radius, int localEffect, int globalEffect, int width, int height, int revenue, Size size, int xPos, int yPos, int buildTime) : Utility(20, electricity, water, symbol, radius, localEffect, globalEffect, width, height, revenue, size, xPos, yPos, buildTime) {
+    //TODO - change value of output
+}
+
+PowerPlant::PowerPlant(PowerPlant* powerPlant) : Utility(powerPlant) {
+
+}
+
 PowerPlant::~PowerPlant() {}
 
-PowerPlant::PowerPlant(int electricity, int water, std::string symbol, int radius, int localEffect, int globalEffect, int width, int height, int revenue, Size size, int xPos, int yPos, int buildTime) : Utility(electricity, water, symbol, radius, localEffect, globalEffect, width, height, revenue, size, xPos, yPos, buildTime)
-{
-    setOutput(20); //TODO - change value
+void PowerPlant::update() {
+    if (!isBuilt()) {
+        updateBuildState();
+    }
 }
 
-void PowerPlant::update()
-{
-    //TODO
-}
-
-Entity* PowerPlant::clone()
-{
-    Entity* e = new PowerPlant(electricityConsumption, waterConsumption, symbol, effectRadius, localEffectStrength, globalEffectStrength, width, height, revenue, size, xPosition, yPosition, state->getBuildTime());
-    return e;
+Entity* PowerPlant::clone() {
+    return new PowerPlant(this);
 }
