be1ea2ac5b9e018d96d5d4032c4288f3c610c7b3
Reece Jordaan <treesnek@gmail.com>
2024-10-18 10:51:36 +0200

    Made additions to WasteManagement

--- src/entities/utility/wastemanagement/WasteManagement.cpp
+++ src/entities/utility/wastemanagement/WasteManagement.cpp
@@ -1,20 +1,21 @@
 #include "WasteManagement.h"
 
-WasteManagement::WasteManagement() {}
-WasteManagement::~WasteManagement() {}
-
-WasteManagement::WasteManagement(int electricity, int water, std::string symbol, int radius, int localEffect, int globalEffect, int width, int height, int revenue, Size size, int xPos, int yPos, int buildTime) : Utility(electricity, water, symbol, radius, localEffect, globalEffect, width, height, revenue, size, xPos, yPos, buildTime)
-{
-    setOutput(20); //TODO - change value
+WasteManagement::WasteManagement(int electricity, int water, std::string symbol, int radius, int localEffect, int globalEffect, int width, int height, int revenue, Size size, int xPos, int yPos, int buildTime) : Utility(20, electricity, water, symbol, radius, localEffect, globalEffect, width, height, revenue, size, xPos, yPos, buildTime) {
+    //TODO - change value of output (1st param in utility constructor)
 }
 
-void WasteManagement::update()
-{
-    //TODO
+WasteManagement::WasteManagement(WasteManagement* wasteManagement) : Utility(wasteManagement) {
+
 }
 
-Entity* WasteManagement::clone()
-{
-    Entity* e = new WasteManagement(electricityConsumption, waterConsumption, symbol, effectRadius, localEffectStrength, globalEffectStrength, width, height, revenue, size, xPosition, yPosition, state->getBuildTime());
-    return e;
+WasteManagement::~WasteManagement() {
+
+}
+
+void WasteManagement::update() {
+    
+}
+
+Entity* WasteManagement::clone() {
+    return new WasteManagement(this);
 }
