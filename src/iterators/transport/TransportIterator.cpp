5e9bd75331bd8fd0c04b26d5eacaddd027c96c75
0ptmi0s <aryanmohanlall@gmail.com>
2024-10-19 16:06:03 +0200

    Completed Transport Iterator

--- src/iterators/transport/TransportIterator.cpp
+++ src/iterators/transport/TransportIterator.cpp
@@ -2,3 +2,57 @@
 
 TransportIterator::TransportIterator() {}
 TransportIterator::~TransportIterator() {}
+
+TransportIterator::TransportIterator(std::vector<std::vector<Entity*>> &grid){
+    this->grid = grid;
+    this->currRow = this->grid.begin();
+    this->curr = currRow->begin();
+}
+
+void TransportIterator::first(){
+    this->currRow = this->grid.begin();
+    this->curr = currRow->begin();
+    bool found = false;
+
+    for(;currRow != this->grid.end(); currRow++){
+        for(curr = currRow->begin(); curr != currRow->end(); curr++){
+            Transport* transport = dynamic_cast<Transport*>(*curr);
+
+            if(transport){found = true;break;}
+        }
+        if(found)break;
+    }
+}
+
+void TransportIterator::next(){
+    bool found = false;
+
+    for(;currRow != this->grid.end(); currRow++){
+        for(curr = currRow->begin(); curr != currRow->end(); curr++){
+            Transport* transport = dynamic_cast<Transport*>(*curr);
+
+            if(transport){found = true;break;}
+        }
+        if(found)break;
+    }
+}
+
+bool TransportIterator::hasNext(){
+    bool found = false;
+    std::vector<std::vector<Entity*>>::iterator tempRow = this->currRow;
+    std::vector<Entity*>::iterator tempCurr = this->curr;
+
+        for(;tempRow != this->grid.end(); tempRow++){
+        for(tempCurr = tempRow->begin(); tempCurr != tempRow->end(); tempCurr++){
+            Transport* transport = dynamic_cast<Transport*>(*tempCurr);
+
+            if(transport){found = true;break;}
+        }
+        if(found)break;
+    }
+    return found;
+}
+
+Transport* TransportIterator::current(){
+    return dynamic_cast<Transport*>(*this->curr);
+}
--- src/iterators/transport/TransportIterator.h
+++ src/iterators/transport/TransportIterator.h
@@ -2,12 +2,19 @@
 #define TRANSPORTITERATOR_H
 
 #include "iterators/base/Iterator.h"
+#include "../entities/transport/Transport.h"
 
 class TransportIterator : public Iterator
 {
 public:
     TransportIterator();
     ~TransportIterator();
+
+    TransportIterator(std::vector<std::vector<Entity*>> &grid);
+    void first();
+    void next();
+    bool hasNext();
+    Transport* current();
 };
 
 #endif // TRANSPORTITERATOR_H
