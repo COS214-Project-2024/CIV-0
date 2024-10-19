7e9dbd231548a6601e8901b5f9778caa6ce32829
0ptmi0s <aryanmohanlall@gmail.com>
2024-10-19 14:44:02 +0200

    Completed City Iterator

--- src/iterators/city/CityIterator.cpp
+++ src/iterators/city/CityIterator.cpp
@@ -2,3 +2,57 @@
 
 CityIterator::CityIterator() {}
 CityIterator::~CityIterator() {}
+
+CityIterator::CityIterator(std::vector<std::vector<Entity*>> &grid){
+    this->grid = grid;
+    this->currRow = this->grid.begin();
+    this->curr = currRow->begin();
+}
+
+void CityIterator::first(){
+    this->currRow = this->grid.begin();
+    this->curr = currRow->begin();
+    bool found = false;
+
+    for(;currRow != this->grid.end(); currRow++){
+        for(curr = currRow->begin(); curr != currRow->end(); curr++){
+            Entity* entity = dynamic_cast<Entity*>(*curr);
+
+            if(entity){found = true;break;}
+        }
+        if(found)break;
+    }
+}
+
+void CityIterator::next(){
+    bool found = false;
+
+    for(;currRow != this->grid.end(); currRow++){
+        for(curr = currRow->begin(); curr != currRow->end(); curr++){
+            Entity* entity = dynamic_cast<Entity*>(*curr);
+
+            if(entity){found = true;break;}
+        }
+        if(found)break;
+    }
+}
+
+bool CityIterator::hasNext(){
+    bool found = false;
+    std::vector<std::vector<Entity*>>::iterator tempRow = this->currRow;
+    std::vector<Entity*>::iterator tempCurr = this->curr;
+
+        for(;tempRow != this->grid.end(); tempRow++){
+        for(tempCurr = tempRow->begin(); tempCurr != tempRow->end(); tempCurr++){
+            Entity* entity = dynamic_cast<Entity*>(*tempCurr);
+
+            if(entity){found = true;break;}
+        }
+        if(found)break;
+    }
+    return found;
+}
+
+Entity* CityIterator::current(){
+    return dynamic_cast<Entity*>(*this->curr);
+}
--- src/iterators/city/CityIterator.h
+++ src/iterators/city/CityIterator.h
@@ -2,12 +2,19 @@
 #define CITYITERATOR_H
 
 #include "iterators/base/Iterator.h"
+#include "../city/City.h"
 
 class CityIterator : public Iterator
 {
 public:
     CityIterator();
     ~CityIterator();
+
+    CityIterator(std::vector<std::vector<Entity*>> &grid);
+    void first();
+    void next();
+    bool hasNext();
+    Entity* current();
 };
 
 #endif // CITYITERATOR_H
