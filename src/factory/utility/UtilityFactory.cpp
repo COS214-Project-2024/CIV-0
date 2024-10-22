7594ccae3f49ff2324928213dd3bfd01c466273a
RaiderRoss <aidan1.mck@gmail.com>
2024-10-22 21:57:08 +0200

    Done intial setup of UtilityFactory

--- src/factory/utility/UtilityFactory.cpp
+++ src/factory/utility/UtilityFactory.cpp
@@ -1,4 +1,54 @@
 #include "UtilityFactory.h"
 
-UtilityFactory::UtilityFactory() {}
-UtilityFactory::~UtilityFactory() {}
+UtilityFactory::UtilityFactory() {
+
+}
+UtilityFactory::~UtilityFactory() {
+
+}
+
+Entity* UtilityFactory::createSmallEntity(EntityType type, int xPos, int yPos) {
+	return nullptr;
+}
+
+Entity * UtilityFactory::createMediumEntity(EntityType type, int xPos, int yPos)
+{
+return nullptr;
+}
+
+Entity * UtilityFactory::createLargeEntity(EntityType type, int xPos, int yPos)
+{
+return nullptr;
+}
+
+Entity* UtilityFactory::createPowerPlant(Size size, int xPos, int yPos) {
+	return nullptr;
+}
+
+Entity* UtilityFactory::createWaterSupply(Size size, int xPos, int yPos) {
+	return nullptr;
+}
+
+Entity * UtilityFactory::createWasteManagement(Size size, int xPos, int yPos)
+{
+return nullptr;
+}
+
+Entity * UtilityFactory::createSewageSystem(Size size, int xPos, int yPos)
+{
+return nullptr;
+}
+
+Entity* UtilityFactory::createConcreteProducer(Size size, int xPos, int yPos) {
+	return nullptr;
+}
+
+Entity * UtilityFactory::createStoneProducer(Size size, int xPos, int yPos)
+{
+return nullptr;
+}
+
+Entity * UtilityFactory::createWoodProducer(Size size, int xPos, int yPos)
+{
+return nullptr;
+}
