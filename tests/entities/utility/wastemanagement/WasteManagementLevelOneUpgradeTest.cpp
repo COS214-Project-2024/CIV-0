558467784ff84c26668b52fa1c7e316a2272b736
Reece Jordaan <treesnek@gmail.com>
2024-10-22 21:56:47 +0200

    Update WasteManagement tests

--- tests/entities/utility/wastemanagement/WasteManagementLevelOneUpgradeTest.cpp
+++ tests/entities/utility/wastemanagement/WasteManagementLevelOneUpgradeTest.cpp
@@ -1,40 +1,41 @@
 #include "doctest.h"
 #include "entities/utility/wastemanagement/WasteManagementLevelOneUpgrade.h"
 #include "entities/utility/wastemanagement/WasteManagement.h"
+#include "utils/ConfigManager.h"
 
 TEST_SUITE("WasteManagementLevelOneUpgrade Tests") {
-    // TEST_CASE("WasteManagementLevelOneUpgrade constructor") {
-    //     WasteManagement basePlant(ConfigManager::getEntityConfig(EntityType::WASTEMANAGMENT, Size::SMALL), Size::SMALL, 10, 10);
-    //     WasteManagementLevelOneUpgrade upgrade(&basePlant);
+    TEST_CASE("WasteManagementLevelOneUpgrade constructor") {
+        WasteManagement basePlant(ConfigManager::getEntityConfig(EntityType::WASTEMANAGMENT, Size::SMALL), Size::SMALL, 10, 10);
+        WasteManagementLevelOneUpgrade upgrade(&basePlant);
 
-    //     CHECK(upgrade.getOutput() == 1000);
-    // }
+        CHECK(upgrade.getOutput() == 20 * 2);
+    }
 
-    // TEST_CASE("WasteManagementLevelOneUpgrade update()") {
-    //     WasteManagement basePlant(ConfigManager::getEntityConfig(EntityType::WASTEMANAGMENT, Size::SMALL), Size::SMALL, 10, 10);
-    //     WasteManagementLevelOneUpgrade upgrade(&basePlant);
+    TEST_CASE("WasteManagementLevelOneUpgrade update()") {
+        WasteManagement basePlant(ConfigManager::getEntityConfig(EntityType::WASTEMANAGMENT, Size::MEDIUM), Size::MEDIUM, 10, 10);
+        WasteManagementLevelOneUpgrade upgrade(&basePlant);
 
-    //     upgrade.update();
-    //     CHECK(upgrade.getOutput() == 800);
-    // }
+        upgrade.update();
+        CHECK(upgrade.getOutput() == 20 * 2);
+    }
 
-    // TEST_CASE("WasteManagementLevelOneUpgrade clone()") {
-    //     WasteManagement basePlant(ConfigManager::getEntityConfig(EntityType::WASTEMANAGMENT, Size::SMALL), Size::SMALL, 10, 10);
-    //     WasteManagementLevelOneUpgrade upgrade(&basePlant);
+    TEST_CASE("WasteManagementLevelOneUpgrade clone()") {
+        WasteManagement basePlant(ConfigManager::getEntityConfig(EntityType::WASTEMANAGMENT, Size::LARGE), Size::LARGE, 10, 10);
+        WasteManagementLevelOneUpgrade upgrade(&basePlant);
 
-    //     Entity* clonedUpgrade = upgrade.clone();
-    //     WasteManagementLevelOneUpgrade* cloned = dynamic_cast<WasteManagementLevelOneUpgrade*>(clonedUpgrade);
+        Entity* clonedUpgrade = upgrade.clone();
+        WasteManagementLevelOneUpgrade* cloned = dynamic_cast<WasteManagementLevelOneUpgrade*>(clonedUpgrade);
         
-    //     REQUIRE(cloned != nullptr);
-    //     CHECK(cloned->getOutput() == 700);
+        REQUIRE(cloned != nullptr);
+        CHECK(cloned->getOutput() == 20 * 2);
 
-    //     delete clonedUpgrade;
-    // }
+        delete clonedUpgrade;
+    }
 
-    // TEST_CASE("WasteManagementLevelOneUpgrade getOutput()") {
-    //     WasteManagement basePlant(ConfigManager::getEntityConfig(EntityType::WASTEMANAGMENT, Size::SMALL), Size::SMALL, 10, 10);
-    //     WasteManagementLevelOneUpgrade upgrade(&basePlant);
+    TEST_CASE("WasteManagementLevelOneUpgrade getOutput()") {
+        WasteManagement basePlant(ConfigManager::getEntityConfig(EntityType::WASTEMANAGMENT, Size::SMALL), Size::SMALL, 10, 10);
+        WasteManagementLevelOneUpgrade upgrade(&basePlant);
 
-    //     CHECK(upgrade.getOutput() == 1200);
-    // }
+        CHECK(upgrade.getOutput() == 20 * 2);
+    }
 }
