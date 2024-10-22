cb8a3ee78ed09fbc037f1261f9b2324e9bb48bbb
RaiderRoss <aidan1.mck@gmail.com>
2024-10-22 21:56:35 +0200

    Done intial setup of transport factories

--- src/factory/transport/TransportFactory.cpp
+++ src/factory/transport/TransportFactory.cpp
@@ -1,4 +1,32 @@
 #include "TransportFactory.h"
 
-TransportFactory::TransportFactory() {}
-TransportFactory::~TransportFactory() {}
+TransportFactory::TransportFactory() {
+
+}
+TransportFactory::~TransportFactory() {
+    
+}
+
+Entity* TransportFactory::createSmallEntity(EntityType type, int xPos, int yPos) {
+	return nullptr;
+}
+
+Entity* TransportFactory::createMediumEntity(EntityType type, int xPos, int yPos) {
+	return nullptr;
+}
+
+Entity* TransportFactory::createLargeEntity(EntityType type, int xPos, int yPos) {
+	return nullptr;
+}
+
+Entity* TransportFactory::createBusStop(Size size, int xPos, int yPos) {
+	return nullptr;
+}
+
+Entity* TransportFactory::createTrainStation(Size size, int xPos, int yPos) {
+	return nullptr;
+}
+
+Entity* TransportFactory::createAirport(Size size, int xPos, int yPos) {
+	return nullptr;
+}
--- src/factory/transport/TransportFactory.h
+++ src/factory/transport/TransportFactory.h
@@ -1,13 +1,21 @@
 #ifndef TRANSPORTFACTORY_H
 #define TRANSPORTFACTORY_H
 
-#include "factory/base/EntityFactory.h"
+#include "EntityFactory.h"
+#include "Size.h"
 
-class TransportFactory : EntityFactory
-{
-public:
-    TransportFactory();
-    ~TransportFactory();
+class TransportFactory : EntityFactory {
+   public:
+	TransportFactory();
+	~TransportFactory();
+	virtual Entity* createSmallEntity(EntityType type, int xPos, int yPos);
+	virtual Entity* createMediumEntity(EntityType type, int xPos, int yPos);
+	virtual Entity* createLargeEntity(EntityType type, int xPos, int yPos);
+
+   private:
+	Entity* createBusStop(Size size, int xPos, int yPos);
+	Entity* createTrainStation(Size size, int xPos, int yPos);
+	Entity* createAirport(Size size, int xPos, int yPos);
 };
 
-#endif // TRANSPORTFACTORY_H
+#endif
