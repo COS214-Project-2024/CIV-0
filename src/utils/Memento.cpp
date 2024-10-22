a361d6c9434de02ba7129b5812f4ea93fd0e6e0c
Shaylin Govender <shaylin.sg@gmail.com>
2024-10-22 17:39:19 +0200

    Memento Class Implementation

--- src/utils/Memento.cpp
+++ src/utils/Memento.cpp
@@ -1,4 +1,20 @@
 #include "Memento.h"
 
-Memento::Memento() {}
-Memento::~Memento() {}
+Memento::Memento(const std::string& name, const std::string& detail)
+    : name(name), detail(detail) {}
+
+std::string Memento::getName() const {
+    return name;
+}
+
+std::string Memento::getDetail() const {
+    return detail;
+}
+
+void Memento::setName(const std::string& name) {
+    this->name = name;
+}
+
+void Memento::setDetail(const std::string& detail) {
+    this->detail = detail;
+}
--- src/utils/Memento.h
+++ src/utils/Memento.h
@@ -1,11 +1,19 @@
 #ifndef MEMENTO_H
 #define MEMENTO_H
 
-class Memento
-{
+#include <string>
+
+class Memento {
+private:
+    std::string name;
+    std::string detail;
+
 public:
-    Memento();
-    ~Memento();
+    Memento(const std::string& name, const std::string& detail);
+    std::string getName() const;
+    std::string getDetail() const;
+    void setName(const std::string& name);
+    void setDetail(const std::string& detail);
 };
 
-#endif // MEMENTO_H
+#endif
