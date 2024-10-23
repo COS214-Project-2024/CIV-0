6842c3facae18103c45e6fe13c5b23e0ab04766e
RaiderRoss <aidan1.mck@gmail.com>
2024-10-23 10:00:29 +0200

    Done initial set up of service building factory

--- src/factory/building/ServiceBuildingFactory.cpp
+++ src/factory/building/ServiceBuildingFactory.cpp
@@ -2,3 +2,32 @@
 
 ServiceBuildingFactory::ServiceBuildingFactory() {}
 ServiceBuildingFactory::~ServiceBuildingFactory() {}
+
+Entity* ServiceBuildingFactory::createSmallEntity(EntityType type, int xPos, int yPos) {
+	return nullptr;
+}
+
+Entity * ServiceBuildingFactory::createMediumEntity(EntityType type, int xPos, int yPos)
+{
+return nullptr;
+}
+
+Entity * ServiceBuildingFactory::createLargeEntity(EntityType type, int xPos, int yPos)
+{
+return nullptr;
+}
+
+Entity * ServiceBuildingFactory::createHospital(Size size, int xPos, int yPos)
+{
+return nullptr;
+}
+
+Entity * ServiceBuildingFactory::createPoliceStation(Size size, int xPos, int yPos)
+{
+return nullptr;
+}
+
+Entity * ServiceBuildingFactory::createSchool(Size size, int xPos, int yPos)
+{
+return nullptr;
+}
