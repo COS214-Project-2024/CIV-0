d96b37bb8bac518a8a59e86d8df540dfe0a2d5be
RaiderRoss <aidan1.mck@gmail.com>
2024-10-23 10:08:41 +0200

    Done initial setup of economic building factory

--- src/factory/building/EconomicBuildingFactory.cpp
+++ src/factory/building/EconomicBuildingFactory.cpp
@@ -2,3 +2,27 @@
 
 EconomicBuildingFactory::EconomicBuildingFactory() {}
 EconomicBuildingFactory::~EconomicBuildingFactory() {}
+
+Entity* EconomicBuildingFactory::createSmallEntity(EntityType type, int xPos, int yPos) {
+	return nullptr;
+}
+
+Entity * EconomicBuildingFactory::createMediumEntity(EntityType type, int xPos, int yPos)
+{
+return nullptr;
+}
+
+Entity * EconomicBuildingFactory::createLargeEntity(EntityType type, int xPos, int yPos)
+{
+return nullptr;
+}
+
+Entity * EconomicBuildingFactory::createFactory(Size size, int xPos, int yPos)
+{
+return nullptr;
+}
+
+Entity * EconomicBuildingFactory::createShoppingMall(Size size, int xPos, int yPos)
+{
+return nullptr;
+}
