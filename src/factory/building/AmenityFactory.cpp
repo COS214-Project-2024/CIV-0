c080049bec59fd72c92d9b543c891e75fd94b0fd
RaiderRoss <aidan1.mck@gmail.com>
2024-10-23 09:57:47 +0200

    Done initial setup of amenity factory

--- src/factory/building/AmenityFactory.cpp
+++ src/factory/building/AmenityFactory.cpp
@@ -2,3 +2,32 @@
 
 AmenityFactory::AmenityFactory() {}
 AmenityFactory::~AmenityFactory() {}
+
+Entity* AmenityFactory::createSmallEntity(EntityType type, int xPos, int yPos) {
+	return nullptr;
+}
+
+Entity * AmenityFactory::createMediumEntity(EntityType type, int xPos, int yPos)
+{
+return nullptr;
+}
+
+Entity * AmenityFactory::createLargeEntity(EntityType type, int xPos, int yPos)
+{
+return nullptr;
+}
+
+Entity * AmenityFactory::createPark(Size size, int xPos, int yPos)
+{
+return nullptr;
+}
+
+Entity * AmenityFactory::createTheater(Size size, int xPos, int yPos)
+{
+return nullptr;
+}
+
+Entity * AmenityFactory::createMonument(Size size, int xPos, int yPos)
+{
+return nullptr;
+}
--- src/factory/building/AmenityFactory.h
+++ src/factory/building/AmenityFactory.h
@@ -8,6 +8,14 @@ class AmenityFactory : public EntityFactory
 public:
     AmenityFactory();
     ~AmenityFactory();
+    virtual Entity* createSmallEntity(EntityType type, int xPos, int yPos);
+    virtual Entity* createMediumEntity(EntityType type, int xPos, int yPos);
+    virtual Entity* createLargeEntity(EntityType type, int xPos, int yPos);
+    private:
+    Entity* createPark(Size size, int xPos, int yPos);
+    Entity* createTheater(Size size, int xPos, int yPos);
+    Entity* createMonument(Size size, int xPos, int yPos);
+
 };
 
-#endif // AMENITYFACTORY_H
+#endif
