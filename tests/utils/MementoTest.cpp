54c221a9ff34dc76a194194bd34ef5bd1f7c3424
Shaylin Govender <shaylin.sg@gmail.com>
2024-10-24 13:51:07 +0200

    Added a doctest.h file beacuse there was not one and it was giving me errors. Unit tests for Memento

--- tests/utils/MementoTest.cpp
+++ tests/utils/MementoTest.cpp
@@ -1,6 +1,28 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
+#include "Memento.h"
 
-TEST_CASE("MementoTest test") {
-    // Add your test cases here
-    CHECK(1 == 1);
+/**
+ * @brief Test creation and retrieval of Memento details.
+ */
+TEST_CASE("Test Memento Creation") {
+    Memento memento("Policy1", "Detail1");
+    
+    // Test initial state
+    CHECK(memento.getName() == "Policy1");
+    CHECK(memento.getDetail() == "Detail1");
+}
+
+/**
+ * @brief Test modifying the state of Memento.
+ */
+TEST_CASE("Test Memento Modification") {
+    Memento memento("Policy1", "Detail1");
+    
+    // Modify state
+    memento.setName("Policy2");
+    memento.setDetail("Detail2");
+
+    CHECK(memento.getName() == "Policy2");
+    CHECK(memento.getDetail() == "Detail2");
 }
