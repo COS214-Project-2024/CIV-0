2eddb35dbb3be3c432b9d1aacf72c8eec8d9569b
Reece Jordaan <treesnek@gmail.com>
2024-10-24 09:39:16 +0200

    Added EconomicBuilding class comments

--- src/entities/building/base/Building.h
+++ src/entities/building/base/Building.h
@@ -3,15 +3,65 @@
 
 #include "entities/base/Entity.h"
 
+/**
+ * @class Building
+ * @brief Abstract class representing a general building entity in the city builder/manager game.
+ * 
+ * This class serves as the base class for all types of buildings. It inherits from the Entity class 
+ * and provides an interface for common building functionalities.
+ */
 class Building : public Entity
 {
 public:
+    /**
+     * @brief Default constructor for the Building class.
+     * 
+     * Initializes a new instance of the Building class with default values.
+     */
     Building();
+
+    /**
+     * @brief Parameterized constructor for the Building class.
+     * 
+     * @param ec The configuration object containing general entity properties.
+     * @param size The size of the building entity.
+     * @param xPos The x-coordinate position of the building on the map.
+     * @param yPos The y-coordinate position of the building on the map.
+     * 
+     * Initializes a new instance of the Building class with specific values.
+     */
     Building(EntityConfig ec, Size size, int xPos, int yPos);
+
+    /**
+     * @brief Copy constructor for the Building class.
+     * 
+     * @param building A pointer to an existing Building object to copy from.
+     * 
+     * Creates a new Building instance as a copy of the provided object.
+     */
     Building(Building* building);
+
+    /**
+     * @brief Destructor for the Building class.
+     * 
+     * Ensures proper cleanup of resources when a Building object is destroyed.
+     */
     virtual ~Building();
 
+    /**
+     * @brief Updates the state of the building entity.
+     * 
+     * A pure virtual function that must be implemented by derived classes to handle changes in the building's state.
+     */
     virtual void update() = 0;
+
+    /**
+     * @brief Clones the building entity.
+     * 
+     * Returns a deep copy of the current Building object.
+     * 
+     * @return A pointer to the newly cloned Building entity.
+     */
     virtual Entity* clone() = 0;
 };
 
--- src/entities/building/economic/Factory.h
+++ src/entities/building/economic/Factory.h
@@ -3,15 +3,64 @@
 
 #include "EconomicBuilding.h"
 
+/**
+ * @class Factory
+ * @brief Concrete class representing a factory in the city builder/manager game.
+ * 
+ * Factory is a type of EconomicBuilding that produces goods and supports industrial activities.
+ */
 class Factory : public EconomicBuilding
 {
 public:
+    /**
+     * @brief Default constructor for the Factory class.
+     * 
+     * Initializes a new instance of the Factory class with default values.
+     */
     Factory();
+
+    /**
+     * @brief Parameterized constructor for the Factory class.
+     * 
+     * @param ec The configuration object containing general entity properties.
+     * @param size The size of the factory entity.
+     * @param xPos The x-coordinate position of the factory on the map.
+     * @param yPos The y-coordinate position of the factory on the map.
+     * 
+     * Initializes a new instance of the Factory class with specific values.
+     */
     Factory(EntityConfig ec, Size size, int xPos, int yPos);
+
+    /**
+     * @brief Copy constructor for the Factory class.
+     * 
+     * @param factory A pointer to an existing Factory object to copy from.
+     * 
+     * Creates a new Factory instance as a copy of the provided object.
+     */
     Factory(Factory* factory);
+
+    /**
+     * @brief Destructor for the Factory class.
+     * 
+     * Ensures proper cleanup of resources when a Factory object is destroyed.
+     */
     ~Factory();
 
+    /**
+     * @brief Updates the state of the factory entity.
+     * 
+     * This function handles changes in the factory's state.
+     */
     void update();
+
+    /**
+     * @brief Clones the factory entity.
+     * 
+     * Returns a deep copy of the current Factory object.
+     * 
+     * @return A pointer to the newly cloned Factory entity.
+     */
     Entity* clone();
 };
 
