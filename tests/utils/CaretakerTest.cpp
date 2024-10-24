b66c31682f46e2999658eb9eebf28eb3efa887a6
Shaylin Govender <shaylin.sg@gmail.com>
2024-10-24 13:56:44 +0200

    Unit tests - Caretaker

---
+++ tests/utils/CaretakerTest.cpp
@@ -0,0 +1,27 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+#include "Caretaker.h"
+#include "Memento.h"
+
+/**
+ * @brief Test storing and retrieving Mementos in Caretaker.
+ */
+TEST_CASE("Test Memento Storage in Caretaker") {
+    Caretaker caretaker;
+    
+    Memento* m1 = new Memento("Policy1", "Detail1");
+    Memento* m2 = new Memento("Policy2", "Detail2");
+    
+    caretaker.setMemento(m1);
+    caretaker.setMemento(m2);
+
+    // Retrieve the most recent memento (m2)
+    Memento* retrieved = caretaker.getMemento();
+    
+    CHECK(retrieved->getName() == "Policy2");
+    CHECK(retrieved->getDetail() == "Detail2");
+
+    // Clean up memory
+    delete m1;
+    delete m2;
+}
