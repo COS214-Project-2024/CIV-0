3e25fd5832a9f8e6dbf9536cb42b49343ecf0c57
Reece Jordaan <treesnek@gmail.com>
2024-10-24 14:36:37 +0200

    Added comments to Amenity classes

--- src/entities/building/amenity/Amenity.h
+++ src/entities/building/amenity/Amenity.h
@@ -5,15 +5,58 @@
 #include "entities/base/Subject.h"
 #include "entities/building/residential/ResidentialBuilding.h"
 
+/**
+ * @brief Abstract class representing an amenity building in the city.
+ * 
+ * Amenities provide non-essential but beneficial services, like parks, monuments, or theaters.
+ * It inherits from the Building and Subject classes.
+ */
 class Amenity : public Building, public Subject
 {
 public:
+    /**
+     * @brief Default constructor for the Amenity class.
+     * 
+     * Initializes an empty amenity object.
+     */
     Amenity();
+
+    /**
+     * @brief Parameterized constructor for the Amenity class.
+     * 
+     * @param ec Entity configuration for initializing the amenity.
+     * @param size The size of the amenity.
+     * @param xPos The x-coordinate of the amenity's location.
+     * @param yPos The y-coordinate of the amenity's location.
+     */
     Amenity(EntityConfig ec, Size size, int xPos, int yPos);
+
+    /**
+     * @brief Copy constructor for the Amenity class.
+     * 
+     * @param amenity A pointer to an existing Amenity object to copy from.
+     */
     Amenity(Amenity* amenity);
+
+    /**
+     * @brief Destructor for the Amenity class.
+     * 
+     * Cleans up resources used by the Amenity object.
+     */
     virtual ~Amenity();
 
+    /**
+     * @brief Pure virtual function for updating the state of the amenity.
+     * 
+     * Must be implemented by derived classes to handle amenity-specific updates.
+     */
     virtual void update() = 0;
+
+    /**
+     * @brief Pure virtual function for cloning the amenity.
+     * 
+     * @return A pointer to a deep copy of the amenity object.
+     */
     virtual Entity* clone() = 0;
 };
 
--- src/entities/building/amenity/Monument.h
+++ src/entities/building/amenity/Monument.h
@@ -3,15 +3,58 @@
 
 #include "Amenity.h"
 
+/**
+ * @brief Class representing a monument in the city.
+ * 
+ * The Monument class provides a decorative or commemorative structure to enhance the city's appeal.
+ * It inherits from the Amenity class.
+ */
 class Monument : public Amenity
 {
 public:
+    /**
+     * @brief Default constructor for the Monument class.
+     * 
+     * Initializes an empty monument object.
+     */
     Monument();
+
+    /**
+     * @brief Parameterized constructor for the Monument class.
+     * 
+     * @param ec Entity configuration for initializing the monument.
+     * @param size The size of the monument.
+     * @param xPos The x-coordinate of the monument's location.
+     * @param yPos The y-coordinate of the monument's location.
+     */
     Monument(EntityConfig ec, Size size, int xPos, int yPos);
+
+    /**
+     * @brief Copy constructor for the Monument class.
+     * 
+     * @param monument A pointer to an existing Monument object to copy from.
+     */
     Monument(Monument* monument);
+
+    /**
+     * @brief Destructor for the Monument class.
+     * 
+     * Cleans up resources used by the Monument object.
+     */
     ~Monument();
 
+    /**
+     * @brief Updates the state of the monument entity.
+     * 
+     * Handles changes in the monument's state.
+     */
     void update();
+
+    /**
+     * @brief Clones the monument entity.
+     * 
+     * @return A pointer to a deep copy of the Monument object.
+     */
     Entity* clone();
 };
 
