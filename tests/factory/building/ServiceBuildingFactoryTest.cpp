d2621e7cbde632d9386ea89730ca069962552d68
RaiderRoss <aidan1.mck@gmail.com>
2024-10-26 00:52:48 +0200

    Finished all testing

--- tests/factory/building/ServiceBuildingFactoryTest.cpp
+++ tests/factory/building/ServiceBuildingFactoryTest.cpp
@@ -1,6 +1,53 @@
 #include "doctest.h"
-
+#include "factory/building/ServiceBuildingFactory.h"
+#include "utils/EntityType.h"
 TEST_CASE("ServiceBuildingFactoryTest test") {
-    // Add your test cases here
-    CHECK(1 == 1);
+    ServiceBuildingFactory serviceBuildingFactory;
+    Entity* entity = serviceBuildingFactory.createSmallEntity(EntityType::HOSPITAL, 0, 0);
+    CHECK(entity != nullptr);
+    CHECK(entity->getXPosition() == 0);
+    CHECK(entity->getYPosition() == 0);
+    delete entity;
+    entity = serviceBuildingFactory.createMediumEntity(EntityType::HOSPITAL, 0, 0);
+    CHECK(entity != nullptr);
+    CHECK(entity->getXPosition() == 0);
+    CHECK(entity->getYPosition() == 0);
+    delete entity;
+    entity = serviceBuildingFactory.createLargeEntity(EntityType::HOSPITAL, 0, 0);
+    CHECK(entity != nullptr);
+    CHECK(entity->getXPosition() == 0);
+    CHECK(entity->getYPosition() == 0);
+    delete entity;
+    entity = serviceBuildingFactory.createSmallEntity(EntityType::POLICESTATION, 0, 0);
+    CHECK(entity != nullptr);
+    CHECK(entity->getXPosition() == 0);
+    CHECK(entity->getYPosition() == 0);
+    delete entity;
+    entity = serviceBuildingFactory.createMediumEntity(EntityType::POLICESTATION, 0, 0);
+    CHECK(entity != nullptr);
+    CHECK(entity->getXPosition() == 0);
+    CHECK(entity->getYPosition() == 0);
+    delete entity;
+    entity = serviceBuildingFactory.createLargeEntity(EntityType::POLICESTATION, 0, 0);
+    CHECK(entity != nullptr);
+    CHECK(entity->getXPosition() == 0);
+    CHECK(entity->getYPosition() == 0);
+    delete entity;
+    entity = serviceBuildingFactory.createSmallEntity(EntityType::SCHOOL, 0, 0);
+    CHECK(entity != nullptr);
+    CHECK(entity->getXPosition() == 0);
+    CHECK(entity->getYPosition() == 0);
+    delete entity;
+    entity = serviceBuildingFactory.createMediumEntity(EntityType::SCHOOL, 0, 0);
+    CHECK(entity != nullptr);
+    CHECK(entity->getXPosition() == 0);
+    CHECK(entity->getYPosition() == 0);
+    delete entity;
+    entity = serviceBuildingFactory.createLargeEntity(EntityType::SCHOOL, 0, 0);
+    CHECK(entity != nullptr);
+    CHECK(entity->getXPosition() == 0);
+    CHECK(entity->getYPosition() == 0);
+    delete entity;
+    
+
 }
