9b1976d9ecbb2e6d6fbe2642f5f858770d7bc64c
RaiderRoss <aidan1.mck@gmail.com>
2024-10-26 01:04:42 +0200

    Done docs for all factories

--- src/factory/building/ServiceBuildingFactory.h
+++ src/factory/building/ServiceBuildingFactory.h
@@ -6,17 +6,84 @@
 #include "entities/building/service/PoliceStation.h"
 #include "entities/building/service/School.h"
 
-class ServiceBuildingFactory : EntityFactory
-{
+/**
+ * @class ServiceBuildingFactory
+ * @brief A factory class for creating service buildings such as hospitals, police stations, and schools.
+ * 
+ * This class inherits from EntityFactory and provides methods for creating small, medium,
+ * and large service buildings of different types.
+ */
+class ServiceBuildingFactory : public EntityFactory {
 public:
+    /**
+     * @brief Constructor for the ServiceBuildingFactory class.
+     */
     ServiceBuildingFactory();
+
+    /**
+     * @brief Destructor for the ServiceBuildingFactory class.
+     */
     ~ServiceBuildingFactory();
-    virtual Entity* createSmallEntity(EntityType type, int xPos, int yPos);
-    virtual Entity* createMediumEntity(EntityType type, int xPos, int yPos);
-    virtual Entity* createLargeEntity(EntityType type, int xPos, int yPos);
-    private:
+
+    /**
+     * @brief Creates a small service building of the specified type at the given position.
+     * 
+     * @param type The type of service building to create.
+     * @param xPos The x-coordinate of the building's position.
+     * @param yPos The y-coordinate of the building's position.
+     * @return A pointer to the created Entity.
+     */
+    virtual Entity* createSmallEntity(EntityType type, int xPos, int yPos) override;
+
+    /**
+     * @brief Creates a medium service building of the specified type at the given position.
+     * 
+     * @param type The type of service building to create.
+     * @param xPos The x-coordinate of the building's position.
+     * @param yPos The y-coordinate of the building's position.
+     * @return A pointer to the created Entity.
+     */
+    virtual Entity* createMediumEntity(EntityType type, int xPos, int yPos) override;
+
+    /**
+     * @brief Creates a large service building of the specified type at the given position.
+     * 
+     * @param type The type of service building to create.
+     * @param xPos The x-coordinate of the building's position.
+     * @param yPos The y-coordinate of the building's position.
+     * @return A pointer to the created Entity.
+     */
+    virtual Entity* createLargeEntity(EntityType type, int xPos, int yPos) override;
+
+private:
+    /**
+     * @brief Creates a hospital of the specified size at the given position.
+     * 
+     * @param size The size of the hospital to create.
+     * @param xPos The x-coordinate of the hospital's position.
+     * @param yPos The y-coordinate of the hospital's position.
+     * @return A pointer to the created Entity.
+     */
     Entity* createHospital(Size size, int xPos, int yPos);
+
+    /**
+     * @brief Creates a police station of the specified size at the given position.
+     * 
+     * @param size The size of the police station to create.
+     * @param xPos The x-coordinate of the police station's position.
+     * @param yPos The y-coordinate of the police station's position.
+     * @return A pointer to the created Entity.
+     */
     Entity* createPoliceStation(Size size, int xPos, int yPos);
+
+    /**
+     * @brief Creates a school of the specified size at the given position.
+     * 
+     * @param size The size of the school to create.
+     * @param xPos The x-coordinate of the school's position.
+     * @param yPos The y-coordinate of the school's position.
+     * @return A pointer to the created Entity.
+     */
     Entity* createSchool(Size size, int xPos, int yPos);
 };
 
--- src/factory/industry/IndustryFactory.h
+++ src/factory/industry/IndustryFactory.h
@@ -1,22 +1,90 @@
 #ifndef INDUSTRYFACTORY_H
 #define INDUSTRYFACTORY_H
+
 #include "entities/industry/concreteproducer/ConcreteProducer.h"
 #include "entities/industry/stoneproducer/StoneProducer.h"
 #include "entities/industry/woodproducer/WoodProducer.h"
 #include "factory/base/EntityFactory.h"
 
-class IndustryFactory : EntityFactory {
-   public:
-	IndustryFactory();
-	~IndustryFactory();
-	virtual Entity* createSmallEntity(EntityType type, int xPos, int yPos);
-	virtual Entity* createMediumEntity(EntityType type, int xPos, int yPos);
-	virtual Entity* createLargeEntity(EntityType type, int xPos, int yPos);
+/**
+ * @class IndustryFactory
+ * @brief A factory class for creating industrial entities like concrete, stone, and wood producers.
+ * 
+ * This class inherits from EntityFactory and provides methods for creating small, medium,
+ * and large industrial entities of different types.
+ */
+class IndustryFactory : public EntityFactory {
+public:
+    /**
+     * @brief Constructor for the IndustryFactory class.
+     */
+    IndustryFactory();
 
-   private:
-	Entity* createConcreteProducer(Size size, int xPos, int yPos);
-	Entity* createStoneProducer(Size size, int xPos, int yPos);
-	Entity* createWoodProducer(Size size, int xPos, int yPos);
+    /**
+     * @brief Destructor for the IndustryFactory class.
+     */
+    ~IndustryFactory();
+
+    /**
+     * @brief Creates a small industrial entity of the specified type at the given position.
+     * 
+     * @param type The type of industrial entity to create.
+     * @param xPos The x-coordinate of the entity's position.
+     * @param yPos The y-coordinate of the entity's position.
+     * @return A pointer to the created Entity.
+     */
+    virtual Entity* createSmallEntity(EntityType type, int xPos, int yPos);
+
+    /**
+     * @brief Creates a medium industrial entity of the specified type at the given position.
+     * 
+     * @param type The type of industrial entity to create.
+     * @param xPos The x-coordinate of the entity's position.
+     * @param yPos The y-coordinate of the entity's position.
+     * @return A pointer to the created Entity.
+     */
+    virtual Entity* createMediumEntity(EntityType type, int xPos, int yPos);
+
+    /**
+     * @brief Creates a large industrial entity of the specified type at the given position.
+     * 
+     * @param type The type of industrial entity to create.
+     * @param xPos The x-coordinate of the entity's position.
+     * @param yPos The y-coordinate of the entity's position.
+     * @return A pointer to the created Entity.
+     */
+    virtual Entity* createLargeEntity(EntityType type, int xPos, int yPos);
+
+private:
+    /**
+     * @brief Creates a concrete producer of the specified size at the given position.
+     * 
+     * @param size The size of the concrete producer to create.
+     * @param xPos The x-coordinate of the producer's position.
+     * @param yPos The y-coordinate of the producer's position.
+     * @return A pointer to the created Entity.
+     */
+    Entity* createConcreteProducer(Size size, int xPos, int yPos);
+
+    /**
+     * @brief Creates a stone producer of the specified size at the given position.
+     * 
+     * @param size The size of the stone producer to create.
+     * @param xPos The x-coordinate of the producer's position.
+     * @param yPos The y-coordinate of the producer's position.
+     * @return A pointer to the created Entity.
+     */
+    Entity* createStoneProducer(Size size, int xPos, int yPos);
+
+    /**
+     * @brief Creates a wood producer of the specified size at the given position.
+     * 
+     * @param size The size of the wood producer to create.
+     * @param xPos The x-coordinate of the producer's position.
+     * @param yPos The y-coordinate of the producer's position.
+     * @return A pointer to the created Entity.
+     */
+    Entity* createWoodProducer(Size size, int xPos, int yPos);
 };
 
-#endif	// INDUSTRYFACTORY_H
+#endif // INDUSTRYFACTORY_H
