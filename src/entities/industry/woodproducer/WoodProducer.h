01439abbaefb5011f445e169902b760891c9f39a
CartographySilence <u23668882@tuks.co.za>
2024-10-25 12:49:04 +0200

    Saving so i can checkout

--- src/entities/industry/woodproducer/WoodProducer.h
+++ src/entities/industry/woodproducer/WoodProducer.h
@@ -6,11 +6,10 @@
 class WoodProducer : public Industry
 {
 public:
-    WoodProducer();
+    WoodProducer(WoodProducer* WoodProducer);
     WoodProducer(int electricity, int water, std::string symbol, int radius, int localEffect, int globalEffect,
                  int width, int height, int revenue, Size size, int xPos, int yPos, int buildTime);
     virtual ~WoodProducer();
-
     void update();
     Entity *clone();
 };
