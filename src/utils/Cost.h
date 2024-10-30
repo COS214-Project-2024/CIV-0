0a469147dfd0f18b24b4883120a980b7f5a91007
Reece Jordaan <treesnek@gmail.com>
2024-10-30 07:41:13 +0200

    Overloaded the Cost == operator for easier testing

--- src/utils/Cost.h
+++ src/utils/Cost.h
@@ -10,6 +10,14 @@ struct Cost
 
     Cost(int money = 0, int wood = 0, int stone = 0, int concrete = 0)
         : moneyCost(money), woodCost(wood), stoneCost(stone), concreteCost(concrete) {}
+
+    // Overload the == operator
+    bool operator==(const Cost& other) const {
+        return moneyCost == other.moneyCost &&
+               woodCost == other.woodCost &&
+               stoneCost == other.stoneCost &&
+               concreteCost == other.concreteCost;
+    }
 };
 
 #endif // COST_H
