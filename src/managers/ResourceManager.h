36c2d60a47b73572f7fd60590c234f49aa4aac49
CartographySilence <u23668882@tuks.co.za>
2024-10-31 12:38:49 +0200

    Added class definitions for resource manager

--- src/managers/ResourceManager.h
+++ src/managers/ResourceManager.h
@@ -1,11 +1,26 @@
 #ifndef RESOURCEMANAGER_H
 #define RESOURCEMANAGER_H
 
+#include "src/entities/industry/base/Industry.h"
 class ResourceManager
 {
 public:
+
     ResourceManager();
     ~ResourceManager();
+    int calculateMoneyMade(); //ask richard how to get the money
+    int calculateWoodMade();
+    int calculateStoneMade();
+    int calculateConcreteMade();
+    
+    std::vector<Industry*> getAllIndustryBuildings();
+    std::vector<Industry*> getAllConcreteProducers();
+    std::vector<Industry*> getAllStoneProducers();
+    std::vector<Industry*> getAllWoodProducers();
+    bool canAffordUpgrade(Industry* industry);
+    bool canUpgrade(Industry* industry);
+    bool upgrade(Industry* industry);
+
 };
 
 #endif // RESOURCEMANAGER_H
