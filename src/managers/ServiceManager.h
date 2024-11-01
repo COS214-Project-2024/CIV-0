162a7992fec0cac94c250aa267f4e482e2103b08
RaiderRoss <aidan1.mck@gmail.com>
2024-11-01 14:00:36 +0200

    Fixed ServiceManager

--- src/managers/ServiceManager.h
+++ src/managers/ServiceManager.h
@@ -8,11 +8,28 @@
 #include "city/City.h"
 /**
  * @class ServiceManager
- * @brief Manages the creation and handling of service buildings within the system.
+ * @brief Manages the creation and destruction of service buildings.
  *
- * The ServiceManager class is responsible for managing service-related operations,
- * including building and configuring various types of service buildings based on
- * specified parameters such as type, size, and position.
+ * The ServiceManager class is responsible for constructing and initializing
+ * service buildings of various types and sizes at specified coordinates.
+ *
+ * @fn ServiceManager::ServiceManager()
+ * @brief Constructs a new ServiceManager object.
+ *
+ * @fn ServiceManager::~ServiceManager()
+ * @brief Destroys the ServiceManager object.
+ *
+ * @fn bool ServiceManager::buildService(EntityType type, Size size, int xPos, int yPos)
+ * @brief Builds a new service building.
+ *
+ * Creates and initializes a service building of the specified type and size at the given
+ * position coordinates.
+ *
+ * @param type The entity type of the service building to create.
+ * @param size The size of the service building.
+ * @param xPos The x-coordinate position of the service building.
+ * @param yPos The y-coordinate position of the service building.
+ * @return A boolean indicating success or failure of the building creation.
  */
 class ServiceManager
 {
@@ -27,19 +44,8 @@ public:
      */
     ~ServiceManager();
 
-    /**
-     * @brief Builds a new service building.
-     *
-     * Creates and initializes a service building of the specified type and size at the given
-     * position coordinates.
-     *
-     * @param type The entity type of the service building to create.
-     * @param size The size of the service building.
-     * @param xPos The x-coordinate position of the service building.
-     * @param yPos The y-coordinate position of the service building.
-     * @return A pointer to the created ServiceBuilding object.
-     */
-    void buildService(EntityType type, Size size, int xPos, int yPos);
+
+    bool buildService(EntityType type, Size size, int xPos, int yPos);
 };
 
 #endif // SERVICEMANAGER_H
