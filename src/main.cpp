e921dd1f61a21edf9c4c9f7d27bebe48f336576e
Reece Jordaan <treesnek@gmail.com>
2024-11-03 23:43:18 +0200

    Added comments to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include "city/CivZero.h"
 
+/**
+ * @brief This is the main. It does main things.
+ * @return 0 if everything goes to plan.
+ */
 int main()
 {
     CivZero::instance().startGame(true);
